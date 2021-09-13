f5facd294c7f8246d55590f8c90b32cb5e886081
Ivan Krivosheev <ikrivosheev@ptsecurity.com>
2021-09-13 20:07:15 +0300

    bump: vendored feature uses v4.1.2

--- yara-sys/build.rs
+++ yara-sys/build.rs
@@ -210,7 +210,7 @@ mod build {
     }
 }
 
-#[cfg(feature = "bundled-4_1_1")]
+#[cfg(feature = "bundled-4_1_2")]
 mod bindings {
     use std::env;
     use std::fs;
@@ -218,8 +218,8 @@ mod bindings {
 
     pub fn add_bindings() {
         let binding_file = match env::var("CARGO_CFG_TARGET_FAMILY").unwrap().as_ref() {
-            "unix" => "yara-4.1.1-unix.rs",
-            "windows" => "yara-4.1.1-windows.rs",
+            "unix" => "yara-4.1.2-unix.rs",
+            "windows" => "yara-4.1.2-windows.rs",
             f => panic!("no bundled bindings for family {}", f),
         };
         let out_dir = env::var("OUT_DIR").expect("$OUT_DIR should be defined");
@@ -229,7 +229,7 @@ mod bindings {
     }
 }
 
-#[cfg(not(feature = "bundled-4_1_1"))]
+#[cfg(not(feature = "bundled-4_1_2"))]
 mod bindings {
     extern crate bindgen;
 
