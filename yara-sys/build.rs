079ba7105946f3b1f99df2bb83102dce95b8c8e2
Ivan Krivosheev <py.krivosheev@gmail.com>
2022-08-20 15:32:16 +0300

    feat: upgrade yara to 4.2.3

--- yara-sys/build.rs
+++ yara-sys/build.rs
@@ -292,7 +292,7 @@ mod bindings {
     use std::path::PathBuf;
 
     pub fn add_bindings() {
-        let binding_file = format!("yara-4.2.2-{}.rs", env::var("TARGET").unwrap());
+        let binding_file = format!("yara-4.2.3-{}.rs", env::var("TARGET").unwrap());
         let binding_path = PathBuf::from("bindings").join(binding_file);
         let out_dir = env::var("OUT_DIR").expect("$OUT_DIR should be defined");
         let out_path = PathBuf::from(out_dir).join("bindings.rs");
