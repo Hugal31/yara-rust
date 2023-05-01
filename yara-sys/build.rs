9d972f3ad8269e7fd1763377da68e13eaa8bb75e
Ivan Krivosheev <py.krivosheev@gmail.com>
2023-05-01 12:19:31 +0300

    feat: Upgrade yara up to 4.3.1

--- yara-sys/build.rs
+++ yara-sys/build.rs
@@ -340,14 +340,14 @@ mod build {
     }
 }
 
-#[cfg(feature = "bundled-4_3_0")]
+#[cfg(feature = "bundled-4_3_1")]
 mod bindings {
     use std::env;
     use std::fs;
     use std::path::PathBuf;
 
     pub fn add_bindings() {
-        let binding_file = format!("yara-4.3.0-{}.rs", env::var("TARGET").unwrap());
+        let binding_file = format!("yara-4.3.1-{}.rs", env::var("TARGET").unwrap());
         let binding_path = PathBuf::from("bindings").join(binding_file);
         let out_dir = env::var("OUT_DIR").expect("$OUT_DIR should be defined");
         let out_path = PathBuf::from(out_dir).join("bindings.rs");
@@ -363,7 +363,7 @@ mod bindings {
     }
 }
 
-#[cfg(not(feature = "bundled-4_3_0"))]
+#[cfg(not(feature = "bundled-4_3_1"))]
 mod bindings {
     use std::env;
     use std::path::PathBuf;
